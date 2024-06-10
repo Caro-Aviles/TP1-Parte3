bc8310057850c810e8b7dfb98a2b07573d7005da
Caro-Aviles <c.avilesaviles@uandresbello.edu>
2024-06-10 19:20:59 -0400

    Obteniendo el numero total de clientes unicos

--- Pandas.py
+++ Pandas.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 #CARGANDO LOS 5 ARCHIVOS CSV EN DATAFRAMES DE PANDAS
 df_customers = pd.read_csv('U:/CAVILES/CAPACITACION/2024/DATA_ENGINEER/TP1-Parte3/ecommerce_customers_dataset.csv')
 df_order_items = pd.read_csv('U:/CAVILES/CAPACITACION/2024/DATA_ENGINEER/TP1-Parte3/ecommerce_order_items_dataset.csv')
@@ -7,9 +8,16 @@ df_order_payments = pd.read_csv('U:/CAVILES/CAPACITACION/2024/DATA_ENGINEER/TP1-
 df_orders = pd.read_csv('U:/CAVILES/CAPACITACION/2024/DATA_ENGINEER/TP1-Parte3/ecommerce_orders_dataset.csv')
 df_products = pd.read_csv('U:/CAVILES/CAPACITACION/2024/DATA_ENGINEER/TP1-Parte3/ecommerce_products_dataset.csv')
 
+
 #ESTABLECIENDO LAS PK EN EL DATAFRAME
 df_customers.set_index('customer_id', inplace=True)
 df_order_items.set_index('order_id', inplace=True)
 df_order_payments.set_index('order_id', inplace=True)
 df_orders.set_index('order_id', inplace=True)
 df_products.set_index('product_id', inplace=True)
+
+
+#OBTENIENDO EL NUMERO TOTAL DE CLIENTES UNICOS
+num_clientes_unicos =  len(df_customers.index)
+#IMPRIMIENDO EL NUMERO TOTAL DE CLIENTES UNICOS
+print ("El numero total de clientes unicos es: ", num_clientes_unicos)
